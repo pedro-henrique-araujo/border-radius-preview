bc78d8bd0791cb149b18c6b1e6de714c65a5905e
Pedro <pedrohenriqueribeirodearaujo01@gmail.com>
2020-11-09 15:17:55 -0300

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import './App.css'
 
 function App() {
+  const borderRadiusTarget = useRef<HTMLDivElement>(null)
+  const [borderTopLeftRadius, setBorderTopLeftRadius] = useState<string>('15')
+  const [borderTopRightRadius, setBorderTopRightRadius] = useState<string>('15')
+  const [borderBottomLeftRadius, setBorderBottomLeftRadius] = useState<string>('15')
+  const [borderBottomRightRadius, setBorderBottomRightRadius] = useState<string>('15')
+
+  useEffect(() => {
+    borderRadiusTarget.current?.style.setProperty('border-top-left-radius', `${borderTopLeftRadius}px`)
+  }, [borderTopLeftRadius])
+
+  useEffect(() => {
+    borderRadiusTarget.current?.style.setProperty('border-top-right-radius', `${borderTopRightRadius}px`)
+  }, [borderTopRightRadius])
+
+  useEffect(() => {
+    borderRadiusTarget.current?.style.setProperty('border-bottom-left-radius', `${borderBottomLeftRadius}px`)
+  }, [borderBottomLeftRadius])
+
+  useEffect(() => {
+    borderRadiusTarget.current?.style.setProperty('border-bottom-right-radius', `${borderBottomRightRadius}px`)
+  }, [borderBottomRightRadius])
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Border Radius Preview</h1>
+      <main id="content">
+        <div className="top left">
+          <input 
+            className="text-align-right"
+            value={borderTopLeftRadius}
+            type="number"
+            onChange={event => setBorderTopLeftRadius(event.target.value)} />
+        </div>
+        <div className="top right">
+          <input            
+            value={borderTopRightRadius} 
+            type="number"
+            onChange={event => setBorderTopRightRadius(event.target.value)}/>
+        </div>
+        <div className="center">
+          <div ref={borderRadiusTarget} id="border-radius-target"></div>
+        </div>
+        <div className="bottom left">
+          <input 
+            className="text-align-right" 
+            value={borderBottomLeftRadius}
+            type="number"
+            onChange={event => setBorderBottomLeftRadius(event.target.value)} />
+        </div>
+        <div className="bottom right">
+          <input
+            value={borderBottomRightRadius} 
+            type="number"
+            onChange={event => setBorderBottomRightRadius(event.target.value)}/>
+        </div>
+      </main>     
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
